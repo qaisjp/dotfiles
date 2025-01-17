d27bbdd24e1c6080d1a4508eb4eae2852657ecc1
qaisjp <qaisjp@gmail.com>
2025-01-16 19:08:20 -0500

    remove eventtap in favour of skhd

--- hammerspoon/app-toggle.lua
+++ hammerspoon/app-toggle.lua
@@ -14,76 +14,11 @@ function toggleBundle(bundle)
     end
 end
 
-eventTap = hs.eventtap.new(
-    {
-        -- hs.eventtap.event.types.flagsChanged,
-        -- hs.eventtap.event.types.keyDown,
-        hs.eventtap.event.types.systemDefined
-    },
-    function(event)
-        -- print("Eventtap")
-
-        local key = event:systemKey()
-        -- print()
-        -- print("Keypress:")
-        -- print("keycode", inspect(event:getKeyCode()))
-        -- print("systemKey", inspect(key))
-        -- print("mousecount", hs.mouse.count())
-        if key.down and key.key == "BRIGHTNESS_DOWN" then
-            if not shouldIntercept() then return false end
-            -- print("flags", event:rawFlags())
-
-            -- Don't do our thing if shift is held
-            -- if event:getFlags().shift then
-                -- return false
-            -- end
-
-            -- Don't do our thing if held down
-            if key['repeat'] then
-                return false -- Don't intercept
-            end
-
-            local front = hs.application.frontmostApplication()
-            if front:name() == "iTerm2" then
-                front:hide()
-            else
-                hs.application.launchOrFocus("iTerm.app")
-            end
-
-            -- print('property is', inspect({event:getProperty(hs.eventtap.event.properties.keyboardEventKeyboardType)}))
-            -- must still return false, otherwise repeat doesn't work
-            return true
-        end
-
-
-        if key.down and key.key == "BRIGHTNESS_UP" then
-            if not shouldIntercept() then return false end
-            -- print("flags", event:rawFlags())
-
-            -- Don't do our thing if shift is held
-            -- if event:getFlags().shift then
-                -- return false
-            -- end
-
-            -- Don't do our thing if held down
-            if key['repeat'] then
-                return false -- Don't intercept
-            end
-
-            local front = hs.application.frontmostApplication()
-            -- print("frontapp is", front)
-            if front:name() == "Code" then
-                hs.application.launchOrFocus("Google Chrome.app")
-            else
-                hs.application.launchOrFocus("Visual Studio Code.app")
-            end
-
-            -- print('property is', inspect({event:getProperty(hs.eventtap.event.properties.keyboardEventKeyboardType)}))
-            -- must still return false, otherwise repeat doesn't work
-            return true
-        end
-
-        -- Don't intercept
-        return false
+function toggleChromeOrVscode()
+    local front = hs.application.frontmostApplication()
+    if front:name() == "Code" then
+        hs.application.launchOrFocus("Google Chrome.app")
+    else
+        hs.application.launchOrFocus("Visual Studio Code.app")
     end
-):start()
+end
