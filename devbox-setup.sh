d5919b5926f92fb825086fc2fa1e904b50999022
Mulan Ramani <34253999+MulanRamani@users.noreply.github.com>
2025-05-06 15:29:09 -0400

    Update devbox-setup.sh

--- devbox-setup.sh
+++ devbox-setup.sh
@@ -5,11 +5,30 @@ set -euo pipefail
 if ! [[ $(hostname) =~ qa-mydev* ]]; then
   # ----- local -----
 
-  pay sync --no-sync-loop "$HOME/stripe/dotfiles":/home/jez/.dotfiles
+  pay sync --no-sync-loop "$HOME/stripe/dotfiles":/home/qaisjp/.dotfiles
 
-  pay ssh 'cd /home/jez/.dotfiles && ./devbox-setup.sh'
+  pay ssh 'cd /home/qaisjp/.dotfiles && ./devbox-setup.sh'
 
   exit
 fi
 
 # ----- remote -----
+
+
+echo "💻 Installing oh-my-zsh..."
+if [ ! -d ~/.oh-my-zsh ]; then
+  wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ./zsh-installer.sh
+  sudo chmod +x ./zsh-installer.sh
+  ~/zsh-installer.sh --unattended --keep-zshrc
+
+  echo "✅ Successfully installed oh-my-zsh."
+else
+  echo "✅ oh-my-zsh is already installed. Skipping step."
+fi
+
+echo "🦥 Installing fzf..."
+if ! [ -x "$(command -v fzf)" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --bin
+fi
+echo "✅ Successfully installed fzf."
